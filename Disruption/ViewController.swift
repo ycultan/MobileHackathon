3d73bbb168d17e7ac397fd132fd2b7d4027ce96f
Lucy Tan <Lucy@lucy.local>
2017-09-21 11:41:15 -0700

    Initial Commit

---
+++ Disruption/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Disruption
+//
+//  Created by Lucy Tan on 9/21/17.
+//  Copyright © 2017 Lucy Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
